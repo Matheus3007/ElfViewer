6771eeb610c219fa3661c15afb1ba88e8fc0084b
Matheus Rezende <matheusrp2009@hotmail.com>
2023-08-17 15:07:38 -0300

    Added new themes (RuthlessRats, ThoughtfulTux)

---
+++ themes/RuthlessRats.py
@@ -0,0 +1,19 @@
+name = "RuthlessRats"
+theme = {
+    "ULA": (0, 0, 45),                             # Dark Blue
+    "Acesso a memoria": (254, 184, 0),             # Yellow
+
+    "Desvios": (0, 0, 235),                         # Darker Blue
+    "Pilha": (0, 0, 75),                           # Even Darker Blue
+    "Rotacoes": (0, 0, 90),                        # Very Dark Blue
+
+    "Acesso a memoria com PF": (255, 240, 0),      # Light Yellow
+    "Acesso a memoria multiplo": (254, 223, 0),    # Light Yellow (Reusing)
+    "UNDEFINED": (255, 217, 0),                  # White
+    "Coprocessadores": (254, 223, 0),              # Light Yellow (Reusing)
+    "ULA com PF": (254, 138, 0),                   # Orange
+    "Modo supervisor": (0, 0, 75),                 # Even Darker Blue (Reusing)
+    "Operacoes com vetores": (0, 0, 60),           # Darker Blue (Reusing)
+    "Nop": (0, 0, 159),                            # Very Dark Blue (Reusing)
+    "background": (0, 0, 45)                       # Dark Blue
+}
--- themes/ThoughtfulTux.py
+++ themes/ThoughtfulTux.py
@@ -1,16 +1,16 @@
-name = "GrayGrievance"
+name = "ThoughtfulTux"
 theme = {
-    "ULA": (255, 0, 0),                          # COLOR = RED
+    "ULA": (30, 30, 30),                       # COLOR = Dark Gray
     "Acesso a memoria": (70, 70, 70),          # COLOR = Gray
-    "Desvios": (110, 110, 110),                # COLOR = Medium Gray
-    "Pilha": (255, 128, 0),                  # COLOR = Light Gray
-    "Rotacoes":(254, 184, 0),               # COLOR = Very Light Gray
+    "Desvios": (252, 198, 36),                # COLOR = Medium Gray
+    "Pilha": (252, 198, 36),                  # COLOR = Light Gray
+    "Rotacoes": (200, 200, 200),               # COLOR = Very Light Gray
 
     "Acesso a memoria com PF": (210, 210, 210), # COLOR = Off White
     "Acesso a memoria multiplo": (220, 220, 220),# COLOR = Very Light Gray (Reusing)
-    "UNDEFINED": (255, 217, 0),              # COLOR = White
+    "UNDEFINED": (255, 255, 255),              # COLOR = White
     "Coprocessadores": (240, 240, 240),        # COLOR = Very Light Gray (Reusing)
-    "ULA com PF": (255, 128, 0),                 # COLOR = ORANGE
+    "ULA com PF": (180, 180, 180),             # COLOR = Light Gray (Reusing)
     "Modo supervisor": (190, 190, 190),        # COLOR = Light Gray (Reusing)
     "Operacoes com vetores": (160, 160, 160),  # COLOR = Medium Gray (Reusing)
     "Nop": (200, 200, 200),                   # COLOR = Very Light Gray (Reusing)
